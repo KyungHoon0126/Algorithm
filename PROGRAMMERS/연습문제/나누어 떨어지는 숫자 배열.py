ea8a794cb2c4d5cd51e5a0ded2444a5982e10e8c
김경훈 <kidi052812@gmail.com>
2021-04-03 19:44:00 +0900

    [PROGRAMMERS] : 나누어 떨어지는 숫자 배열

---
+++ PROGRAMMERS/연습문제/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,14 @@
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if (i % divisor == 0):
+            answer.append(i)
+    if (len(answer) == 0):
+        answer.append(-1)
+    else:
+        answer.sort()
+
+    return answer
+
+
+solution([3, 2, 6], 10)
